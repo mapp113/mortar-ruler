94ef6603cd7ebf0d3294e802a593835799418bd1
mapp113 <nguyendangducquang@gmail.com>
2026-07-15 15:09:50 +0700

    config pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,55 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'; ``
 
 export default defineConfig({
   base: '/mortar-ruler/',
   plugins: [
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png'],
+      manifest: {
+        name: 'Mortar Ruler',
+        short_name: 'Mortar Ruler',
+        description: 'App calculates the distance between the offset and target lines in a mortar joint.',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'landscape',
+        icons: [
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: 'any'
+          },
+          {
+            src: "/icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+            purpose: 'any'
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: 'any'
+          },
+          {
+            src: "/icons/icon-192x192-maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: 'maskable'
+          },
+          {
+            src: "/icons/icon-512x512-maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
   ],
 })
